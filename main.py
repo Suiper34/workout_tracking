da8781a3a493f76650588ac012380b44f994a3e8
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-12 23:04:31 +0000

    add parameters

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ URL = "https://trackapi.nutritionix.com/v2/natural/exercise"
 headers = {
     'x-app-id': APP_ID,
     'x-app-key': APP_KEY,
-
+}
+parameters = {
+    'query': input('What was the workout today?: '),
+    'age': int(os.environ.get('age'))
 }
 
-workout_response = requests.post(url=URL, headers=headers)
+
+workout_response = requests.post(url=URL, headers=headers, json=parameters)
+workout_response.raise_for_status()
+workout_data = workout_response.json()
+print(workout_data)
